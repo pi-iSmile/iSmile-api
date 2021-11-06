66640b6a03771923268f6066145384891ec85897
narba <hugodds97@gmail.com>
2021-11-06 13:39:23 -0300

    adding user route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,33 @@
 import express from 'express';
+import appRoutes from './http/Routes';
 
-const app = express();
+class Server {
 
-app.get('/', (request, response) => response.json({ hello: 'World' }));
+    private app: express.Application;
 
-app.listen(process.env.PORT || 8080, function() {
-    console.log("Express server listening!!")
-});
+
+    constructor() {
+        this.app = express();
+        this.configuration();
+        this.routes();   
+    }
+
+    public configuration() {
+        this.app.set('port', process.env.PORT || 8080)
+    }
+
+    public routes() {
+        this.app.use('/api', appRoutes)
+        .get('/', (request, response) => response.json({ hello: 'World' }));
+    }
+
+    public start() {
+        this.app.listen(this.app.get('port'), function() {
+            console.log("Express server listening")
+        });
+    }
+}
+
+const server = new Server();
+server.start();
+
