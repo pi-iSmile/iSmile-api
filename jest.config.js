185b990545438005201b362fd140c35881181550
hugodds97 <hugodds97@gmail.com>
2021-11-07 23:14:44 -0300

    Fixing pipeline

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    clearMocks: true,
-    collectCoverage: true,
-    coverageProvider: 'v8',
-    preset: 'ts-jest',
-};
