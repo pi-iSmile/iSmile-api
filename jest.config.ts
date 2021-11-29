f9fc12670ec7b540180393b6f76de044121c72b4
hugodds97 <hugodds97@gmail.com>
2021-11-28 22:10:57 -0300

    changing threshold to test

--- jest.config.ts
+++ jest.config.ts
@@ -43,10 +43,10 @@ export default {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
     },
   },
 
